65f7ce648983e67812fc0fd124f076446902243b
Thomas Weng <thomas.weng11@gmail.com>
2018-07-05 14:22:13 +0800

    Created xmlhttprequest to get data from mongodb

--- journal/app.js
+++ journal/app.js
@@ -6,22 +6,19 @@ app.use("/public", express.static(__dirname + "/public"))
 
 var MongoClient = require('mongodb').MongoClient
 
-// MongoClient.connect('mongodb://localhost:27017/journal_db', function (err, client) {
-//   if (err) throw err
-
-//   var db = client.db('journal_db')
-  
-//   db.collection('entries').find().toArray(function (err, result) {
-//     if (err) throw err
-
-//     console.log(result)
-//   })
-// })
-
-app.get('/',function(req,res){
+app.get('/',function(req, res) {
     res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-// app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/api/v1.0/entries', function(req, res) {
+    MongoClient.connect('mongodb://localhost:27017/journal_db', function (err, client) {
+        if (err) throw err
+        var db = client.db('journal_db')
+        db.collection('entries').find().toArray(function (err, result) {
+            if (err) throw err
+            res.send(result)
+        })
+    })
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
--- journal/public/main.js
+++ journal/public/main.js
@@ -1,4 +1,13 @@
-console.log("hello");
+var entries;
 
-// provide queryable interface to display data from date
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function(){
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+        entries = xmlhttp.response;
+        console.log(entries);
+    }
+}
+xmlhttp.responseType = 'json';
+xmlhttp.open("GET", "http://127.0.0.1:3000/api/v1.0/entries", true);
+xmlhttp.send();
 
