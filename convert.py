56cfff05044aca03d13d80e40c86eee6e35fe714
Thomas Weng <thomas.weng11@gmail.com>
2018-07-05 10:23:22 +0800

    Added beautifulsoup for parsing html

--- convert.py
+++ convert.py
@@ -1,5 +1,6 @@
 import sys
 import datetime
+from bs4 import BeautifulSoup
 
 class Transformer():
     def __init__(self, stdin, fname):
@@ -11,9 +12,14 @@ class Transformer():
         return datetime.date(int('20' + year), int(month), 1)
 
     def transform(self):
-        for line in self.stdin:
-            continue
-            # print(line)
+        soup = BeautifulSoup(self.stdin, 'html.parser')
+        for header in soup.find_all('h1'):
+            date = self.date.replace(day=int(header.get_text()))
+            bullets = header.find_next_sibling('ul')
+            self.parse_bullets(bullets)
+
+    def parse_bullets(self, bullets):
+        pass
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
